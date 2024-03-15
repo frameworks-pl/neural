c08e514486701557bae66b6a4a9404bcace7b753
Janusz Grabis <janusz@frameworks.pl>
2024-03-15 21:37:29 +0100

    neural network class begin

---
+++ neural.py
@@ -0,0 +1,24 @@
+import numpy
+
+class neuralNetwork:
+
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+        self.inputNodes = inputNodes
+        self.hiddennodes = hiddenNodes
+        self.outputnodes = outputNodes
+
+        self.learningRate = learningRate
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+def main():
+    inputNodes = 3
+    hiddenNodes = 3
+    outputNodes = 3
+    learningRate = 0.3
+
+    n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
