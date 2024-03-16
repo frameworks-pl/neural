d36f691ecf1a17568bc0c9d83a8634fdab3bca6b
Janusz Grabis <janusz@frameworks.pl>
2024-03-16 06:31:04 +0100

    calculating weights of input-to-hidden and hidden-to-output

--- neural.py
+++ neural.py
@@ -1,13 +1,20 @@
 import numpy
+import math
 
 class neuralNetwork:
 
     def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
         self.inputNodes = inputNodes
-        self.hiddennodes = hiddenNodes
-        self.outputnodes = outputNodes
+        self.hiddenNodes = hiddenNodes
+        self.outputNodes = outputNodes
 
         self.learningRate = learningRate
+
+        #sampling weights from normal probability distribution centered around zero
+        #and with a standard deviation that is related to number of incoming links into a node
+        #1/sqrt(number of incoming links)
+        self.weightsInputToHidden = numpy.random.normal(0.0, 1/math.sqrt(self.inputNodes), (self.hiddenNodes, self.inputNodes))
+        self.weightsHiddenToOutput = numpy.random.normal(0.0, 1/math.sqrt(self.hiddenNodes), (self.outputNodes, self.hiddenNodes))
 
     def train():
         pass
@@ -22,3 +29,8 @@ def main():
     learningRate = 0.3
 
     n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+    print(n.weightsInputToHidden)
+
+if __name__ == "__main__":
+    main()
+
