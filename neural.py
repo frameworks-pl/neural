a2e4b9beb614bc01b06d865d8d3e26fdeda2b590
Janusz Grabis <janusz@frameworks.pl>
2024-03-31 09:28:21 +0200

    query function (p137)

--- neural.py
+++ neural.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+import scipy.special #sigmoid function is in that lib (called expit())
 
 class neuralNetwork:
 
@@ -16,11 +17,28 @@ class neuralNetwork:
         self.weightsInputToHidden = numpy.random.normal(0.0, 1/math.sqrt(self.inputNodes), (self.hiddenNodes, self.inputNodes))
         self.weightsHiddenToOutput = numpy.random.normal(0.0, 1/math.sqrt(self.hiddenNodes), (self.outputNodes, self.hiddenNodes))
 
+        #activation function using sigmoid function inside (sigmoid == scipy.special.expit)
+        self.activation_function = lambda x: scipy.special.expit(x)
+
     def train():
         pass
 
-    def query():
-        pass
+    def query(self, input_list):
+        #convert inputs list to 2d array
+        inputs = numpy.array(input_list, ndmin=2)
+
+        #calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        #calculate signals emerging from hiden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        #calculate signals info final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        #calculate signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+        
 
 def main():
     inputNodes = 3
